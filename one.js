29a3491a5fbf926b024c2275d66306eec65f0bca
preethisaiyadav <dpreethi703@gmail.com>
2022-07-12 17:42:08 +0530

    first

---
+++ one.js
@@ -0,0 +1,77 @@
+// how to create in array in javascript?
+let a=[]
+let size=[36,38,40,42,44]
+console.log(size[0])
+console.log(size[1])
+console.log(size[2])
+console.log(size[3])
+console.log(size[4])
+console.log(size[5])
+
+//how to access items from arrays in javascript?
+ let x=[10,20,30,40,50,60]
+  console.log(x[0])
+  console.log(x[1])
+  console.log(x[2])
+  console.log(x[3])
+  console.log(x[4])
+  console.log(x[5])
+
+  //how to acess last item access in javascript?;
+  let z=[20,40,60,80,100]
+  let lastDigit=z.slice(-1);
+  console.log(lastDigit)
+  
+
+  // acess all items from arrays?
+ 
+    const techBrands =[
+        {
+            name:"preethi",
+            salary:50000,
+            age:23,
+            realtion:"dughter"
+            
+        },
+        {
+            name:'sai',
+            salary:80000,
+            age:25,
+            realtion:"son"
+        },
+        {
+            name:"venky",
+            salary:80000,
+            age:25,
+            realtion:"son"
+        },
+        {
+            name:"priya",
+            salary:50000,
+            age:21,
+            realtion:"daughter"
+
+        },
+        {
+            name:"anju",
+            salary:50000,
+            age:25,
+            realtion:"daughter"
+
+        },
+        {
+            name:"gowthami",
+            salary:50000,
+            age:24,
+            realtion:"daughter"
+
+        },
+        {
+            name:"ratna",
+            salary:90000,
+            age:45,
+            realtion:"mother"
+
+        }
+    ]
+      console.table(techBrands);
